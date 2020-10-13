edc56a38a2851e8cceb5de9dc8378e83a6582647
Giovanni Di Rosa <giodirosaartes@gmail.com>
2020-10-13 13:05:09 -0300

    grid & tipografia

---
+++ src/views/Grid.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-row>
+     <v-col class="primary" cols="12" sm="6" >
+         <p>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quas veritatis nulla cupiditate fugiat, impedit ex dolor, voluptates deleniti aliquam porro doloremque quis, est at repellendus soluta repellat ullam dignissimos! Obcaecati fugiat, animi fuga iure recusandae at, unde nulla ea nemo dolorem illum quasi. Culpa, non, rerum nulla deserunt corporis laudantium praesentium quae quasi quo provident, odio magnam corrupti. Labore blanditiis nisi fugiat veniam tenetur repellat perferendis nostrum natus quisquam obcaecati excepturi, consequuntur cupiditate quae nobis, a voluptatibus. Eveniet quos nobis esse et quas, aliquid voluptates soluta deleniti dolore excepturi dolorum inventore incidunt asperiores beatae nisi libero tempora saepe! Eligendi, cupiditate debitis. Porro in explicabo officiis! Aliquid, soluta incidunt. Non vel consequatur quam perspiciatis eum, tempora, beatae ab corporis labore in, nulla et possimus! Ab perferendis, necessitatibus id ratione repellat laudantium distinctio eveniet! Nisi cumque incidunt ratione id excepturi reiciendis. Omnis ipsa incidunt vel, excepturi ipsum consequatur. Impedit, quam cupiditate.
+         </p>
+     </v-col>
+        <v-col class="pink darken-2 white--text" cols="12" sm="6" >
+         <p>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quas veritatis nulla cupiditate fugiat, impedit ex dolor, voluptates deleniti aliquam porro doloremque quis, est at repellendus soluta repellat ullam dignissimos! Obcaecati fugiat, animi fuga iure recusandae at, unde nulla ea nemo dolorem illum quasi. Culpa, non, rerum nulla deserunt corporis laudantium praesentium quae quasi quo provident, odio magnam corrupti. Labore blanditiis nisi fugiat veniam tenetur repellat perferendis nostrum natus quisquam obcaecati excepturi, consequuntur cupiditate quae nobis, a voluptatibus. Eveniet quos nobis esse et quas, aliquid voluptates soluta deleniti dolore excepturi dolorum inventore incidunt asperiores beatae nisi libero tempora saepe! Eligendi, cupiditate debitis. Porro in explicabo officiis! Aliquid, soluta incidunt. Non vel consequatur quam perspiciatis eum, tempora, beatae ab corporis labore in, nulla et possimus! Ab perferendis, necessitatibus id ratione repellat laudantium distinctio eveniet! Nisi cumque incidunt ratione id excepturi reiciendis. Omnis ipsa incidunt vel, excepturi ipsum consequatur. Impedit, quam cupiditate.
+         </p>
+     </v-col>
+    </v-row>
+    <v-row class="title white--text">
+     <v-col v-ripple="{ center: true }" class="deep-purple accent-2 text-center elevation-2 pa-12 headline" cols="12"> 
+        <p>
+          Titulo Importante
+        </p>
+     </v-col>
+     </v-row>
+      <v-row class="title white--text">
+     <v-col v-ripple="{ center: true }" class="deep-purple text-center mt-12 pa-16" cols="12"> 
+        <p class="display-1 font-weight-light">
+          Otro titulo
+        </p>
+     </v-col>
+     </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* .title{
+  padding:20px 0px;
+  text-align: center;
+} */
+
+</style>>
+
