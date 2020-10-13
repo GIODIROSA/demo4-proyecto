45b8b2408822138da85ca441abb035de6f74985a
Giovanni Di Rosa <giodirosaartes@gmail.com>
2020-10-13 16:28:44 -0300

    muestra de botones

--- src/views/Botones.vue
+++ src/views/Botones.vue
@@ -32,6 +32,59 @@
         <v-btn icon color="pink">
           <v-icon>mdi-heart</v-icon>
         </v-btn>
+        <v-btn class="mx-2" fab dark small color="info">
+          <v-icon dark>
+            mdi-heart
+          </v-icon>
+        </v-btn>
+        <v-btn class="mx-2" fab dark large color="accent">
+          <v-icon dark>
+            mdi-heart
+          </v-icon>
+        </v-btn>
+        <v-btn color="success">
+          Texto
+          <v-icon>
+            mdi-forum
+          </v-icon>
+        </v-btn>
+        <v-btn color="success mx-4">
+          Texto
+          <v-icon class="ml-2">
+            mdi-forum
+          </v-icon>
+        </v-btn>
+        <v-btn color="cyan lighten-3 mx-4">
+          Texto
+          <v-icon right>
+            mdi-forum
+          </v-icon>
+        </v-btn>
+
+        <v-btn color="cyan lighten-3 mx-4">
+          <v-icon left>
+            mdi-forum
+          </v-icon>
+          Texto
+        </v-btn>
+        <v-btn color="cyan lighten-3 mx-4" outlined>
+          <v-icon left>
+            mdi-forum
+          </v-icon>
+          Texto
+        </v-btn>
+        <v-btn dark color="red accent-3 mx-4 my-4" block>
+          Buy aufield tickets
+          <v-icon right>
+            mdi-heart
+          </v-icon>
+        </v-btn>
+        <v-btn dark color="orange lighten-1 mx-4 my-4" block rounded>
+          Buy aufield tickets
+          <v-icon right>
+            mdi-heart
+          </v-icon>
+        </v-btn>
       </v-col>
     </v-row>
   </v-container>
