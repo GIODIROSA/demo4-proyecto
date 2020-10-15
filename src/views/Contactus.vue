4789cc4aaf18a2e6a9c40c9f5cc912bc7280f031
Giovanni Di Rosa <giodirosaartes@gmail.com>
2020-10-15 00:59:15 -0300

    Crud-comentario

--- src/views/Contactus.vue
+++ src/views/Contactus.vue
@@ -19,10 +19,14 @@
             <p>
               {{ comentario.descripcion }}
             </p>
-            <v-btn color="warning">
+            <v-btn color="warning" @click="editar(index)">
               Editar
             </v-btn>
-            <v-btn color="error" class="mx-4">
+            <v-btn
+              color="error"
+              class="mx-4"
+              @click="eliminarComentario(comentario.id)"
+            >
               Eliminar
             </v-btn>
           </v-card-text>
@@ -54,17 +58,62 @@
         </v-card> -->
       </v-col>
       <!-- DOS -->
-      <v-col cols="12" md="6">
+      <v-col cols="12" md="6" v-if="formAgregar">
         <v-card class="mb-3 pa-3">
           <v-form @submit.prevent="agregarComentario">
-            <v-text-field color="teal lighten-1" label="AufTalk" v-model="titulo">
+            <v-text-field
+              color="teal lighten-1"
+              label="AufTalk"
+              v-model="titulo"
+            >
             </v-text-field>
-            <v-textarea color="deep-purple lighten-2" label="Comentario AufTalk" v-model="descripcion"></v-textarea>
-            <v-btn dark block color="lime accent-4" type="submit">Agregar</v-btn>
+            <v-textarea
+              color="deep-purple lighten-2"
+              label="Comentario AufTalk"
+              v-model="descripcion"
+            ></v-textarea>
+            <v-btn dark block color="lime accent-4" type="submit"
+              >Agregar</v-btn
+            >
+          </v-form>
+        </v-card>
+      </v-col>
+      <!-- update -->
+      <v-col cols="12" md="6" v-if="!formAgregar">
+        <v-card class="mb-3 pa-3">
+          <v-form @submit.prevent="editarComentario">
+            <v-text-field
+              color="teal lighten-1"
+              label="AufTalk"
+              v-model="titulo"
+            >
+            </v-text-field>
+            <v-textarea
+              color="deep-purple lighten-2"
+              label="Comentario AufTalk"
+              v-model="descripcion"
+            ></v-textarea>
+            <v-btn dark block color="warning" type="submit">Editar</v-btn>
           </v-form>
         </v-card>
       </v-col>
     </v-row>
+    <!-- snackbar -->
+    <v-snackbar v-model="snackbar">
+      {{ mensaje }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="red accent-2"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Cerrar
+        </v-btn>
+      </template>
+    </v-snackbar>
+    <!-- final snackbar -->
   </v-container>
 </template>
 
@@ -89,15 +138,47 @@ export default {
       ],
       titulo: "",
       descripcion: "",
+      snackbar: false,
+      mensaje: "",
+      formAgregar: true,
+      indexComentario: "",
     };
-  },//final de data
+  }, //final de data
   methods: {
     agregarComentario() {
-      console.log(this.titulo, this.descripcion);
-      
-    }
-  },
-};
+      if (this.titulo === "" || this.descripcion === "") {
+        this.snackbar = true;
+        this.mensaje = "Debes llenar el comentario!";
+      } else {
+        this.comentarios.push({
+          id: Date.now(),
+          titulo: this.titulo,
+          descripcion: this.descripcion,
+        });
+        this.titulo = "";
+        this.descripcion = "";
+        this.snackbar = true;
+        this.mensaje = "Haz enviado un comentario!!!♫";
+      }
+    },
+    eliminarComentario(id) {
+      this.comentarios = this.comentarios.filter((e) => e.id != id);
+    },
+    editar(index) {
+      this.formAgregar = false;
+      this.titulo = this.comentarios[index].titulo;
+      this.descripcion = this.comentarios[index].descripcion;
+      this.indexComentario = index;
+    },
+    editarComentario() {
+      this.comentarios[this.indexComentario].titulo = this.titulo;
+      this.comentarios[this.indexComentario].descripcion = this.descripcion;
+      this.formAgregar = true;
+      this.titulo = "";
+      this.descripcion = "";
+    },
+  }, //methods
+}; //expor default
 </script>
 
 <style></style>
